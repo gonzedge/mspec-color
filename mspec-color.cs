83dd6d444a361829468d3d554f5a7283aff0e3b0
unknown <Edgar@.(none)>
2011-03-21 09:43:00 -0400

    Fixing exit code for when passing tests...

--- mspec-color.cs
+++ mspec-color.cs
@@ -1,32 +1,34 @@
 using System;
 using System.Threading;
 using System.Diagnostics;
-using System.Text.RegularExpressions;
 
-public class MSpecColor {
-
-    static bool         allTestsPassed       = false;
-    static bool         summaryHasPrinted    = false;
-    static int          msToWaitAfterSummary = 2000;
-    static DateTime     lastLinePrintedAt    = DateTime.MinValue;
-    static Process      process              = null;
-    static Thread       outputProcessor      = null;
+public class MSpecColor
+{
+    static bool allTestsPassed = true;
+    static bool summaryHasPrinted;
+    static int msToWaitAfterSummary = 2000;
+    static DateTime lastLinePrintedAt = DateTime.MinValue;
+    static Process process;
+    static Thread outputProcessor;
     static ConsoleColor originalColor;
 
-    public static void Main(string[] args) {
+    public static void Main(string[] args)
+    {
         originalColor = Console.ForegroundColor;
-	
-	StartProcess(args);
+
+        StartProcess(args);
         StartThread();
         WaitForExit();
-	ResetConsoleColorBackToOriginal();
+        ResetConsoleColorBackToOriginal();
     }
 
-    static void ResetConsoleColorBackToOriginal() {
+    static void ResetConsoleColorBackToOriginal()
+    {
         Console.ForegroundColor = originalColor;
     }
 
-    static void StartThread() {
+    static void StartThread()
+    {
         outputProcessor = new Thread(ProcessOutput);
         outputProcessor.Start();
     }
@@ -40,7 +42,8 @@ public class MSpecColor {
     // we put this in a background thread and only wait for a certain amount of time 
     // after the summary has printed.
     //
-    static void WaitForExit() {
+    static void WaitForExit()
+    {
         while (outputProcessor.IsAlive && ! summaryHasPrinted)
             Thread.Sleep(100); // hang out and wait for the summary to print (or the thread to finish)
 
@@ -48,84 +51,102 @@ public class MSpecColor {
             Thread.Sleep(100); // hang out and wait for X milliseconds to have passed since the last time mspec.exe output a line
 
         // Everything should be done!  Let's tell the Thread that we're aborting it and give it 1 second to finish up ... then exit!
-        if (outputProcessor.IsAlive) {
+        if (outputProcessor.IsAlive)
+        {
             outputProcessor.Abort();
             Thread.Sleep(1000);
         }
 
         // Exit with non-0 exit code unless all of the tests passed
-	ResetConsoleColorBackToOriginal();
+        ResetConsoleColorBackToOriginal();
         if (allTestsPassed)
             Environment.Exit(0);
         else
-            Environment.Exit(1);
+            Environment.Exit(-1);
     }
 
-    static long MsSinceLastLinePrinted { get { return (DateTime.Now.Subtract(lastLinePrintedAt).Ticks / 10000); }}
+    static long MsSinceLastLinePrinted
+    {
+        get { return (DateTime.Now.Subtract(lastLinePrintedAt).Ticks/10000); }
+    }
 
-    static string GetMspecConsoleCommand() {
+    static string GetMspecConsoleCommand()
+    {
         if (Environment.GetEnvironmentVariable("MSPEC_PATH") != null)
             return Environment.GetEnvironmentVariable("MSPEC_PATH");
-        else
-            return "mspec";
+
+        return "mspec";
     }
 
-    static void StartProcess(string[] args) {
+    static void StartProcess(string[] args)
+    {
         process = new System.Diagnostics.Process();
-        process.StartInfo.FileName               = GetMspecConsoleCommand();
-        process.StartInfo.Arguments              = String.Join(" ", args);
-        process.StartInfo.UseShellExecute        = false;
+        process.StartInfo.FileName = GetMspecConsoleCommand();
+        process.StartInfo.Arguments = String.Join(" ", args);
+        process.StartInfo.UseShellExecute = false;
         process.StartInfo.RedirectStandardOutput = true;
-        process.StartInfo.CreateNoWindow         = true;
-        process.StartInfo.WorkingDirectory       = System.IO.Directory.GetCurrentDirectory();
+        process.StartInfo.CreateNoWindow = true;
+        process.StartInfo.WorkingDirectory = System.IO.Directory.GetCurrentDirectory();
         process.Start();
     }
 
-    static void ProcessOutput() {
-	int failed     = 0;
-	int pending    = 0;
-	int passed     = 0;
-        String output  = null;
-        while ((output = process.StandardOutput.ReadLine()) != null) {
+    static void ProcessOutput()
+    {
+        int failed = 0;
+        int pending = 0;
+        int passed = 0;
+        String output = null;
+        while ((output = process.StandardOutput.ReadLine()) != null)
+        {
             lastLinePrintedAt = DateTime.Now;
 
-	    // Failed example
-	    // All lines after this up until another spec or a blank line should be red too
-	    if (output.StartsWith("» ") && output.EndsWith("(FAIL)")) {
+            // Failed example
+            // All lines after this up until another spec or a blank line should be red too
+            if (output.StartsWith("» ") && output.EndsWith("(FAIL)"))
+            {
+                allTestsPassed = false;
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine(output);
-		failed++;
+                failed++;
 
-	    // Pending example
-	    } else if (output.StartsWith("» ") && output.EndsWith("(NOT IMPLEMENTED)")) {
+                // Pending example
+            }
+            else if (output.StartsWith("» ") && output.EndsWith("(NOT IMPLEMENTED)"))
+            {
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine(output);
                 Console.ForegroundColor = originalColor;
-		pending++;
+                pending++;
 
-	    // If it's not a (FAIL) and it's not (NOT IMPLEMENTED), then it's green!
-	    } else if (output.StartsWith("» ")) {
+                // If it's not a (FAIL) and it's not (NOT IMPLEMENTED), then it's green!
+            }
+            else if (output.StartsWith("» "))
+            {
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine(output);
                 Console.ForegroundColor = originalColor;
-		passed++;
+                passed++;
 
-	    // Looks like we're printing out the summary line(s).
-	    // Color the rest of the lines based on whether there were any failures / pending examples.
-	    } else if (output.StartsWith("Contexts: ")) {
-		if (failed > 0)
-		    Console.ForegroundColor = ConsoleColor.Red;
-		else if (pending > 0)
-		    Console.ForegroundColor = ConsoleColor.Yellow;
-		else if (passed > 0)
-		    Console.ForegroundColor = ConsoleColor.Green;
-		else
-		    Console.ForegroundColor = ConsoleColor.Yellow; // no specs?
+                // Looks like we're printing out the summary line(s).
+                // Color the rest of the lines based on whether there were any failures / pending examples.
+            }
+            else if (output.StartsWith("Contexts: "))
+            {
+                if (failed > 0)
+                    Console.ForegroundColor = ConsoleColor.Red;
+                else if (pending > 0)
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                else if (passed > 0)
+                    Console.ForegroundColor = ConsoleColor.Green;
+                else
+                    Console.ForegroundColor = ConsoleColor.Yellow; // no specs?
 
                 Console.WriteLine(output);
 
-            // None of the conditions hit ... simply print out the line!
-            } else {
+                // None of the conditions hit ... simply print out the line!
+            }
+            else
+            {
                 Console.WriteLine(output);
             }
         }
